11a908b4d5fe36d54709462617fa6708b2cc4e44
jamcheri <james.d.cherian@gmail.com>
2021-09-27 21:18:17 -0700

    Cleaned up code blocks into functions, and added feature to keep track of wins/losses.

--- main.js
+++ main.js
@@ -6,50 +6,133 @@ Start with defining problems. Two "people" that make decisions. These would be f
 And then one of three decisions.
 */
 
-//computer function
-function computerPlay() {
-    return random
-}
-
-
 //array of options to throw
-let computerOptions = ["Rock", "Paper", "Scissors"];
+let computerOptions = "";
+let computerChoice = "";
+let userInput = "";
+let result = "";
 
-// choose a random one of the array options. Found this random picking function from stack overflow: https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array 
-let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
-
-console.log("Computer choice is " + computerChoice);
-
-//userinput
-let userInput = prompt("Choose and enter either Rock, Paper, or Scissors: ");
-
-//if userinput is not equal to rock paper or scissors then tell it to re-enter. 
-while (userInput != "Rock" && userInput != "Paper" && userInput != "Scissors") {
-    userInput = prompt("Incorrect choice, please choose and enter either Rock, Paper, or Scissors: ");
-} 
-
-console.log("User choice is " + userInput);
-
-if (userInput === computerChoice) {
-    console.log("Same choice, the result is a draw");
-    alert("Computer choice was " + computerChoice + ". Same choice, the result is a draw");
-} else if (userInput === "Rock" && computerChoice === "Paper") {
-    console.log("Rock loses to Paper, you've lost :(");
-    alert("Computer choice was " + computerChoice + ". Rock loses to Paper, you've lost :(");
-} else if (userInput === "Rock" && computerChoice === "Scissors") {
-    console.log("Rock beats Scissors, you've won! :)");
-    alert("Computer choice was " + computerChoice + ". Rock beats Scissors, you've won! :)");
-} else if (userInput === "Scissors" && computerChoice === "Rock") {
-    console.log("Scissors loses to Rock, you've lost :(")
-    alert("Computer choice was " + computerChoice + ". Scissors loses to Rock, you've lost :(")
-} else if (userInput === "Scissors" && computerChoice === "Paper") {
-    console.log("Scissors beats Paper, you've won! :)")
-    alert("Computer choice was " + computerChoice + ". Scissors beats Paper, you've won! :)")
-} else if (userInput === "Paper" && computerChoice === "Scissors") {
-    console.log("Paper loses to Scissors, you've lost :(")
-    alert("Computer choice was " + computerChoice + ". Paper loses to Scissors, you've lost :(")
-} else {
-    console.log("Paper beats Rock, you've won! :)")
-    alert("Computer choice was " + computerChoice + ". Paper beats Rock, you've won! :)")
+function computerPlay(computerChoice, computerOptions) {
+    // choose a random one of the array options. Found this pick random function from stack overflow: https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array    
+    computerOptions = ["rock", "paper", "scissors"];
+    computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+    
+    return computerChoice;
 }
+
+function userPlay(userInput) {
+    userInput = prompt("Choose and enter either Rock, Paper, or Scissors: ");
+    userInput = userInput.toLowerCase();
+
+    //if userinput is not equal to rock paper or scissors then tell it to re-enter. 
+    while (userInput != "rock" && userInput != "paper" && userInput != "scissors") {
+    userInput = prompt("Incorrect choice, please choose and enter either Rock, Paper, or Scissors: ");
+    userInput = userInput.toLowerCase();
+    }     
+
+    return userInput;
+}
+
+function playRound(userInput, computerChoice, result) {
+    computerChoice = computerPlay();
+    userInput = userPlay();
+
+    if (userInput === computerChoice) {
+        result = "Draw";
+        console.log("Same choice, the result is a draw");
+        alert("Computer choice was " + computerChoice + ". Same choice, the result is a draw");
+    } else if (userInput === "rock" && computerChoice === "paper") {
+        result = "Loss";
+        console.log("Rock loses to paper, you've lost :(");
+        alert("Computer choice was " + computerChoice + ". Rock loses to paper, you've lost :(");
+    } else if (userInput === "rock" && computerChoice === "scissors") {
+        result = "Win";
+        console.log("Rock beats scissors, you've won! :)");
+        alert("Computer choice was " + computerChoice + ". Rock beats scissors, you've won! :)");
+    } else if (userInput === "scissors" && computerChoice === "rock") {
+        result = "Loss";
+        console.log("Scissors loses to rock, you've lost :(")
+        alert("Computer choice was " + computerChoice + ". Scissors loses to rock, you've lost :(")
+    } else if (userInput === "scissors" && computerChoice === "paper") {
+        result = "Win";
+        console.log("Scissors beats paper, you've won! :)")
+        alert("Computer choice was " + computerChoice + ". Scissors beats paper, you've won! :)")
+    } else if (userInput === "paper" && computerChoice === "scissors") {
+        result = "Loss";
+        console.log("Paper loses to scissors, you've lost :(")
+        alert("Computer choice was " + computerChoice + ". Paper loses to scissors, you've lost :(")
+    } else {
+        result = "Win";
+        console.log("Paper beats rock, you've won! :)")
+        alert("Computer choice was " + computerChoice + ". Paper beats rock, you've won! :)")
+    }
+
+    return result;
+}
+
+
+
+
+
+function game(result) {
+    let userScore = 0;
+    let computerScore = 0;
+    
+    result = playRound();
+    if (result === "Loss") {
+        computerScore++;
+    } else if (result === "Win") {
+        userScore++;
+    } else {
+    }
+    alert("Game 1 over! The result was a " + result + ". Your score is now " + userScore + ". Computer score is now " + computerScore + ".");
+
+    result = playRound();
+    if (result === "Loss") {
+        computerScore++;
+    } else if (result === "Win") {
+        userScore++;
+    } else {
+    }
+    alert("Game 2 over! The result was a " + result + ". Your score is now " + userScore + ". Computer score is now " + computerScore + ".");
+
+    result = playRound();
+    if (result === "Loss") {
+        computerScore++;
+    } else if (result === "Win") {
+        userScore++;
+    } else {
+    }
+    alert("Game 3 over! The result was a " + result + ". Your score is now " + userScore + ". Computer score is now " + computerScore + ".");
+
+    result = playRound();
+    if (result === "Loss") {
+        computerScore++;
+    } else if (result === "Win") {
+        userScore++;
+    } else {
+    }
+    alert("Game 4 over! The result was a " + result + ". Your score is now " + userScore + ". Computer score is now " + computerScore + ".");
+
+    result = playRound();
+    if (result === "Loss") {
+        computerScore++;
+    } else if (result === "Win") {
+        userScore++;
+    } else {
+    }
+    alert("Game 5 over! The result was a " + result + ". Your score is now " + userScore + ". Computer score is now " + computerScore + ".");
+
+    if (result > 2) {
+        alert("All games are over. You won " + userScore + " out of 5 games and won the session! :)")
+    } else {
+        alert("All games are over. You won " + userScore + " out of 5 games and lost the session :(")
+    }
+
+}
+
+game();
+
+
+
 
